522b776dcbac19db943aef8b8cdef2cc7bd6f973
lqhjava <44673641+lqhjava@users.noreply.github.com>
2019-04-13 14:24:56 +0800

    test
    反转一个单链表。

---
+++ april/13-1.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode p = head;
+        int temp = 0;
+        int count = 0;
+        while(p != null){
+            p = p.next;
+            count++;
+        }
+        p = head;
+       while(p != null){
+           ListNode q = p;
+           if(count <=0){
+               break;
+           }
+        for(int i =1; i <count; i++){
+            q = q.next;
+        }
+           temp  = q.val;
+           q.val = p.val;
+           p.val = temp;
+           p = p.next;
+           count=count-2;
+       }
+        return head;
+    }
+}

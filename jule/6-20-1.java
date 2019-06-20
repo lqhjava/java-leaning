44a7ce7d389c23b9b0898ef7851a83ef8e0b5d3a
lqhjava <44673641+lqhjava@users.noreply.github.com>
2019-06-20 20:10:27 +0800

    test
    day

---
+++ jule/6-20-1.java
@@ -0,0 +1,14 @@
+package com.lqh.main;
+
+import java.io.IOException;
+
+public class Hello extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().print("这是我第2个Servlet");
+    }
+}

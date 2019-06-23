9c643f7ebcd7719b8fd50927738787b7d2ff5893
lqhjava <44673641+lqhjava@users.noreply.github.com>
2019-06-23 21:44:18 +0800

    test
    day

---
+++ jule/6-23.java
@@ -0,0 +1,16 @@
+package com.lqh.main;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ProertiesTest {
+    public static void main(String[] args) throws Exception {
+        Properties ps = new Properties();
+        File file = new File(
+                "E:\\javaCode\\TestDemo6\\src" +
+                        "\\main\\resources/test.properties");
+        //从外部加载配置
+
+    }
+}

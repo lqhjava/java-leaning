a2cface210b879a0116b79afaf1b873e0c96440a
lqhjava <44673641+lqhjava@users.noreply.github.com>
2019-07-18 15:03:06 +0800

    test
    day

---
+++ july/7-18-1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Printer {
+    public int[] arrayPrint(int[][] arr, int n) {
+        // write code here
+        int[] res = new int[n * n];
+        int index = 0;
+        int startX = 0;
+        int startY = n - 1;
+        while (startX < n) {
+            int x = startX;
+            int y = startY;
+            while (x < n && y < n)
+                res[index++] = arr[x++][y++];
+            if (startY > 0)
+                startY--;
+            else
+                startX++;
+        }
+        return res;
+    }
+}

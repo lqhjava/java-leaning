227a340fdd3fa64fb40fd556a3c3d7128cddcbea
lqhjava <44673641+lqhjava@users.noreply.github.com>
2019-07-28 22:01:49 +0800

    test
    day

---
+++ july/7-28-1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Solution {
+    public int minDistance(String word1, String word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] arr = new int[len1+1][len2+1];
+        for(int i = 0;i <= len1;i++ ){
+            arr[i][0] = i;
+        }
+        for(int i = 0;i <= len2; i++){
+            arr[0][i] = i;
+        }
+        
+        for(int i =1;i <= len1;i++){
+            for(int j = 1;j <= len2;j++){
+                if( word1.charAt(i-1) == word2.charAt(j-1)){
+                    arr[i][j] = 1 + Math.min(arr[i][j-1],arr[i-1][j]);
+                    arr[i][j] = Math.min(arr[i][j],arr[i-1][j-1]);
+                }else{
+                    arr[i][j] = 1 + Math.min(arr[i][j-1],arr[i-1][j]);
+                    arr[i][j] = Math.min(arr[i][j],1+arr[i-1][j-1]);
+                }
+            }
+        }
+        return arr[len1][len2];
+        
+    }
+}

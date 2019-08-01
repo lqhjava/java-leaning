edcb9f4a824257497c75e536b0409e6ff833be9f
lqhjava <44673641+lqhjava@users.noreply.github.com>
2019-08-01 21:50:40 +0800

    test
    day

---
+++ Aug/8-1-2.java
@@ -0,0 +1,37 @@
+package com.ming.vo;
+
+public class Employee {
+
+    private int employeeId;
+    private String employeeName;
+
+
+    public Employee(){
+
+    }
+
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", employeeName='" + employeeName + '\'' +
+                '}';
+    }
+}

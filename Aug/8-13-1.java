f6bb25d0496459d44b27c822d4d7e61259b6870e
lqhjava <44673641+lqhjava@users.noreply.github.com>
2019-08-13 21:23:55 +0800

    test
    day

---
+++ Aug/8-13-1.java
@@ -0,0 +1,18 @@
+package com.lqh.service;
+
+import com.lqh.mapper.UserMapper;
+import com.lqh.po.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    //与mapper层关联  有spring管理  applicationContext-service.xml
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public List<User> querUserService() throws Exception {
+        return userMapper.querUser();
+    }
+}

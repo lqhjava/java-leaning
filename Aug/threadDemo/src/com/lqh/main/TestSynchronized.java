0b01a839d52f0a41475e228fb0fd0b34452c550d
lqhjava <1096389743@qq.com>
2019-09-01 20:10:08 +0800

    day

---
+++ Aug/threadDemo/src/com/lqh/main/TestSynchronized.java
@@ -0,0 +1,14 @@
+package com.lqh.main;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TestSynchronized {
+    public static void deal(String str2,String arr2){
+
+    }
+    public static void main(String[] args) {
+        Map map = new ConcurrentHashMap();
+    }
+}
